54e60e23d81a82ad9d35d980b3dfda9e912d6054
zebing <zebing.fu@desz9.com>
2021-02-04 11:05:19 +0800

    feat: scss and less

--- examples/base/src/components/template/StyleNameCustom.vue
+++ examples/base/src/components/template/StyleNameCustom.vue
@@ -6,10 +6,40 @@
     <div classname="test" :class="{test: true}">class is Object</div>
     <div classname="test" :class="['test']">class is string</div>
     <div classname="test" :class="1 + 1">class is opther</div>
+    <div classname="testscss" :class="1 + 1">
+      class is scss
+      <div classname="testscssin" :class="1 + 1">class is scss in</div>
+    </div>
+    
+    <div classname="testless" :class="1 + 1">
+      class is less
+      <div classname="testlessin" :class="1 + 1">class is lessin</div>
+    </div>
+    
   </div>
 </template>
 <style module>
 .test {
   color: blue;
+}
+</style>
+
+<style lang="scss" module>
+.testscss {
+  color: blue;
+
+  .testscssin {
+    color: blue;
+  }
+}
+</style>
+
+<style lang="less" module>
+.testless {
+  color: blue;
+
+  .testlessin {
+    color: blue;
+  }
 }
 </style>
